12b1bb9c36e9dca4457c4c3e8be8e178e3c83a16
Bishal sarkar <vishalsarkar852@gmail.com>
2025-11-24 22:13:56 +0530

    minor fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -103,7 +103,6 @@ const about = () => {
                     Satisfied clients
                   </h2>
                 </div>
-
                 <div className="flex flex-col items-end justify-center xl:items-center">
                   <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                     <AnimatedNumbers value={50} />+
